9fed344ea1140b0e0e3d4dfca946d0591c52d470
Grace5275 <grace_madson@outlook.com>
2018-07-26 16:34:08 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -18,6 +18,7 @@ first_recycle_bin =  {
 #1. Define a method `address`, that takes a hash as an argument.
 def address(first_recycle_bin)
   
+end
 #This method should return the value of the address key in the hash.
 
 #2. Define a method called `borough` that takes a hash as an argument.
